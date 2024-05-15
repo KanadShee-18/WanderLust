d1ec881b677ebda0ed354938c1f0b0af3bd77de6
Kanad Shee <codewizardkanad@gmail.com>
2024-05-15 16:35:41 +0530

    Added listing validation middleware

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ app.get("/", (req, res) => {
   res.send("Hello! I am root");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+};
 // Index Route:
 app.get(
   "/listings",
@@ -65,12 +73,8 @@ app.get(
 // Create Route:
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
-    let result = listingSchema.validate(req.body);
-    console.log(result);
-    if (result.error) {
-      throw new ExpressError(400, result.error);
-    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -90,10 +94,8 @@ app.get(
 // Update Route:
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for lisitng!");
-    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
