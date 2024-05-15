16a6293963dbd1a694a7eeb1533c558c715f00ca
Kanad Shee <codewizardkanad@gmail.com>
2024-05-15 21:34:34 +0530

    Added error details with the error message

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.get(
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
   if (error) {
-    throw new ExpressError(400, error);
+    let errMsg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(400, errMsg);
   } else {
     next();
   }
